5d267b92bae0a90b51b0798d8f9396f778d02117
Juju <jajwalya.joshi@hotmail.com>
2023-05-31 20:08:48 +0545

    IP request

---
+++ Models/IPResponse.cs
@@ -0,0 +1,18 @@
+using System.Text.Json.Serialization;
+
+
+namespace MvcWebApi.Models{
+
+    public class IPResponse
+    { 
+        [JsonPropertyName("ip")]
+        public string IP { get; set; }
+    }
+
+
+
+}
+
+
+
+
